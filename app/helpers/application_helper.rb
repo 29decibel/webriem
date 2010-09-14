19312ea2b55ac330b8eabdb687a97ef519bb1fad
li dongbin <lidongbin@li-dongbinmatoMac-mini.local>
2010-09-14 22:07:22 +0800

    changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,12 +10,4 @@ module ApplicationHelper
   def link_to_remove(name,f)
     link_to_function(name,"remove_fields(this)")
   end
-  #function to add a link
-  def link_to_add_fields(name, f, association)
-      new_object = f.object.class.reflect_on_association(association).klass.new
-      fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-        render(association.to_s.singularize, :f => builder)
-      end
-      link_to_function(name, h("add_fields(this, \"#{association}\", \"#{fields}\")"))
-    end
 end
