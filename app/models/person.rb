0fa72d8448b99772ea890beba683e6651b57752f
li dongbin <lidongbin@localhost.(none)>
2010-10-05 14:47:21 +0800

    add logo

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,7 +4,7 @@ class Person < ActiveRecord::Base
   belongs_to :duty, :class_name => "Duty", :foreign_key => "duty_id"
   belongs_to :boss, :class_name => "Person", :foreign_key => "boss_id"
   validates_presence_of :duty
-  enum_attr :gender, [['男', 0], ['女', 1]]
+  enum_attr :gender, [['未知',0],['男', 1], ['女', 2]]
   blongs_to_name_attr :dep
   blongs_to_name_attr :duty
 end
