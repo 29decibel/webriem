f0d95afe9028cd1f14f4ade3557ea282850ddacb
29decibel <mike.d.1984@gmail.com>
2010-10-25 01:23:33 -0700

    增加pre_condition，可以传递初始条件进行过滤

--- app/controllers/model_search_controller.rb
+++ app/controllers/model_search_controller.rb
@@ -2,26 +2,24 @@ class ModelSearchController < ApplicationController
   def index
     #get the class name
     @class=eval(params[:class_name])
-    @results=@class.all
+    @results=params[:pre_condition]?@class.where(params[:pre_condition]): @class.scoped
     render :layout=>false if params[:bare]=="true"
   end
   def with
     @class=eval(params[:class_name])
     column=params[:filter][:column_name]
     filter_text=params[:filter_text]
-    if filter_text.blank?
-      @results=@class.all
-    else
+    pre_condition=params[:pre_condition]
+    @results=params[:pre_condition]?@class.where(params[:pre_condition]): @class.scoped
+    if !filter_text.blank?
       if @class.respond_to? :custom_query
         condition_hash=@class.custom_query(column,filter_text)
       end
       #get the result from the custom query
       if condition_hash
-        @results=@class.all(:include=>condition_hash[:include],:conditions=>[condition_hash[:conditions],"%#{filter_text}%"])
+        @results=@results.all(:include=>condition_hash[:include],:conditions=>[condition_hash[:conditions],"%#{filter_text}%"])
       else
-        if @class.respond_to? :custom_select
-          @results=@class.all
-        else
+        if !@class.respond_to? :custom_select
           @results=@class.where("#{column} like ?",'%'+filter_text+'%')
         end
       end
