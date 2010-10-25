b8963f2e0e911abc71647f898585a3ba242a090b
29decibel <mike.d.1984@gmail.com>
2010-10-25 00:44:37 -0700

    1、收款人信息处，将 方向 内容预置为 应还款和应报销。当为应还款时，结算方式等信息均为空，只有金额即可。当为应报销时，后面均自动带出。2、收款人明细处，增加收款调整金额。

--- app/models/reciver.rb
+++ app/models/reciver.rb
@@ -3,5 +3,5 @@ class Reciver < ActiveRecord::Base
   belongs_to :doc_head, :class_name => "DocHead", :foreign_key => "doc_head_id"
   belongs_to :settlement, :class_name => "Settlement", :foreign_key => "settlement_id"
   blongs_to_name_attr :settlement
-  enum_attr :direction, [['+',0],['-', 1]]
+  enum_attr :direction, [['应报销',0],['应还款', 1]]
 end
