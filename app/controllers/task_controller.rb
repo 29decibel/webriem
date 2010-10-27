9f019906f36ace2b537f51bbc084496ee7ea8033
dongbin.li <mike.d.1984@gmail.com>
2010-10-27 23:24:00 +0800

    changes

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -31,7 +31,12 @@ class TaskController < ApplicationController
   		parts=line.split(",")
   		logger.info line
   		#here is create a dep
-  		dep= Dep.create(:u8dep_code=>parts[0].strip,:code=>parts[1].strip,:name=>parts[2].strip,:start_date=>Time.now,:end_date=>Time.new("2999-12-31"))
+  		logger.info parts[0].strip
+  		logger.info parts[1].strip
+  		logger.info parts[2].strip
+  		logger.info Time.new("2999-12-31")
+  		
+  		dep= Dep.new(:u8dep_code=>parts[0].strip,:code=>parts[1].strip,:name=>parts[2].strip,:start_date=>Time.now,:end_date=>Time.new("2999-12-31"))
   		dep.parent_dep=Dep.find_by_code(parts[3].strip)
   		dep.save
   	end
