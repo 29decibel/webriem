d4b584ad621d936eeec0ec53813c6e527389d31f
unknown <dongbin.li@.elong.cn>
2010-10-28 09:51:59 +0800

    changes

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,6 +2,6 @@ class Account < ActiveRecord::Base
   validates_presence_of :name,:account_no
   validates_uniqueness_of :name,:account_no
   def to_s
-    "#{name};#{account_no}"
+    "#{account_no};#{name}"
   end
 end
