2a50f120a321534ac0ec6aa0b5964fdfb9ab156a
unknown <dongbin.li@.elong.cn>
2010-10-29 08:50:09 +0800

    changes from windows

--- app/models/doc_head.rb
+++ app/models/doc_head.rb
@@ -163,8 +163,8 @@ class DocHead < ActiveRecord::Base
     if work_flow_step.is_self_dep==0
       persons=Person.where("dep_id=? and duty_id=?",work_flow_step.dep_id,work_flow_step.duty_id)
     else
-      return nil if self.person==nil
-      dep=self.person.dep
+      return nil if self.approver_id==nil
+      dep=Person.find(self.approver_id).dep
       while dep
         persons=Person.where("dep_id=? and duty_id=?",dep.id,work_flow_step.duty_id)
         break if persons.count>0
