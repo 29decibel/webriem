72ac389189c0f89d5a066923bd3f8e6e9d20b611
dongbin.li <mike.d.1984@gmail.com>
2010-10-31 22:15:22 +0800

    changes

--- public/javascripts/doc_number_input_control.js
+++ public/javascripts/doc_number_input_control.js
@@ -69,7 +69,7 @@ function calculate_ori_amount()
 	var days= parseInt($(this).closest("tr").find("input.travel_days").val());
 	var fee_standard=parseFloat($(this).closest("tr").find("input.fee_standard").val());
 	var other_fee=parseFloat($(this).closest("tr").find("input.other_fee").val());
-	if(other_fee==undefined)
+	if(isNaN(other_fee))
 		other_fee=0;
 	var total=(days*fee_standard+other_fee).toFixed(2);
 	$(this).closest("tr").find("input.doc_ori_amount").val(total);
