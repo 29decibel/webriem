3e665e5b22105570e1f0372ab97447a0cfee53ef
dongbin.li <dongbin.li@elong.corp.com>
2010-11-09 09:43:59 +0800

    changes

--- app/models/common_riem.rb
+++ app/models/common_riem.rb
@@ -4,6 +4,13 @@ class CommonRiem < ActiveRecord::Base
   belongs_to :project
   belongs_to :doc_head
   belongs_to :fee
+  validates_presence_of :fee_id
+  validates_presence_of :dep_id
+  validates_presence_of :project_id
+  validates_presence_of :description
+  validates_presence_of :currency_id
+  validates_presence_of :rate
+  validates_presence_of :ori_amount
   def after_initialize
     self.currency=Currency.find_by_code("RMB")
     if self.currency
--- app/models/rd_extra_work_meal.rb
+++ app/models/rd_extra_work_meal.rb
@@ -3,6 +3,11 @@ class RdExtraWorkMeal < ActiveRecord::Base
     belongs_to :reim_detail 
     belongs_to :currency 
     enum_attr :is_sunday, [['是',0],['否', 1]]
+    validates_presence_of :start_time
+    validates_presence_of :end_time
+    validates_presence_of :reason
+    validates_presence_of :currency_id
+    validates_presence_of :rate
     validates_presence_of :ori_amount
     def after_initialize
       self.currency=Currency.find_by_code("RMB")
