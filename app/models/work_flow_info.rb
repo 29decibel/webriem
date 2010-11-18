fcca5a575d9221b7bb54515eea068ea5727e50cf
ldb <mike.d.1984@gmail.com>
2010-11-18 18:07:35 +0800

    changes

--- app/models/work_flow_info.rb
+++ app/models/work_flow_info.rb
@@ -4,5 +4,5 @@ class WorkFlowInfo < ActiveRecord::Base
   belongs_to :person, :class_name => "Person", :foreign_key => "people_id"
   blongs_to_name_attr :person
   enum_attr :is_ok, [['否',0],['是', 1]]
-  validates_uniqueness_of :people_id, :scope => [:doc_head_id]
+  #validates_uniqueness_of :people_id, :scope => [:doc_head_id]
 end
