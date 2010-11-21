8c137438912029ba13aafe3094f5668535af65a6
dongbin.li <mike.d.1984@gmail.com>
2010-11-21 14:15:41 +0800

    add multi language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,7 @@
 #coding: utf-8
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :set_locale
   before_filter :authenticate_user!,:except => [:sign_in, :sign_up]
-  def set_locale
-    # if params[:locale] is nil then I18n.default_locale will be used
-    if request.port==8012
-      I18n.locale = 'en'
-      logger.info "============================================="
-    end
-  end
   def get_error_messages(record)
     error_msg="\\n"
     record.errors.full_messages.each do |msg|
