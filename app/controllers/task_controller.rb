45ede20fdf082e04bdc6f2f12086015863bc12f3
dongbin.li <mike.d.1984@gmail.com>
2010-11-24 00:18:52 +0800

    changes

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -2,7 +2,7 @@
 class TaskController < ApplicationController
   def my_docs
     if current_user.person
-      redirect_to :controller=>"model_search",:action=>"index",:class_name=>"DocHead",:pre_condition=>"person_id=#{current_user.person.id}",:lookup=>true,:title=>"我的单据",:layout=>true,:outputable=>true,:printable=>true,:user_display_columns=>:my_doc_display_columns,:deletable=>true
+      redirect_to :controller=>"model_search",:action=>"index",:class_name=>"DocHead",:pre_condition=>"person_id=#{current_user.person.id}",:lookup=>true,:title=>I18n.t("menu.my_docs"),:layout=>true,:outputable=>true,:printable=>true,:user_display_columns=>:my_doc_display_columns,:deletable=>true
     else
       render "task/no_rights_error"
     end
@@ -10,7 +10,7 @@ class TaskController < ApplicationController
   #docs need to approve
   def docs_to_approve
     if current_user.person
-      redirect_to :controller=>"model_search",:action=>"index",:class_name=>"DocHead",:pre_condition=>"doc_state=1 and work_flow_step_id is not null",:filter_method=>"docs_to_approve",:lookup=>true,:title=>"需要审批的单据",:multicheck=>true,:checkable=>true,:batch_approve=>true,:layout=>true,:outputable=>true,:printable=>true,:user_display_columns=>:doc_to_approve_display_columns 
+      redirect_to :controller=>"model_search",:action=>"index",:class_name=>"DocHead",:pre_condition=>"doc_state=1 and work_flow_step_id is not null",:filter_method=>"docs_to_approve",:lookup=>true,:title=>I18n.t("menu.docs_to_approve"),:multicheck=>true,:checkable=>true,:batch_approve=>true,:layout=>true,:outputable=>true,:printable=>true,:user_display_columns=>:doc_to_approve_display_columns 
     else
       render "task/no_rights_error"
     end  
@@ -18,7 +18,7 @@ class TaskController < ApplicationController
   #the docs need to pay
   def docs_to_pay
     if current_user.person and current_user.person.person_type and current_user.person.person_type.code=="CA"
-      redirect_to :controller=>"model_search",:action=>"index",:class_name=>"DocHead",:pre_condition=>"doc_state=2",:lookup=>true,:title=>"需要付款的单据",:multicheck=>true,:checkable=>true,:batch_pay=>true,:layout=>true,:outputable=>true,:printable=>true
+      redirect_to :controller=>"model_search",:action=>"index",:class_name=>"DocHead",:pre_condition=>"doc_state=2",:lookup=>true,:title=>I18n.t("menu.docs_to_pay"),:multicheck=>true,:checkable=>true,:batch_pay=>true,:layout=>true,:outputable=>true,:printable=>true
     else
       render "task/no_rights_error"
     end
@@ -26,7 +26,7 @@ class TaskController < ApplicationController
   #the docs that has been approved
   def docs_approved
     if current_user.person
-      redirect_to :controller=>"model_search",:action=>"index",:class_name=>"DocHead",:joins=>"work_flow_infos",:pre_condition=>"work_flow_infos.people_id=#{current_user.person.id}",:lookup=>true,:title=>"审批过的单据",:layout=>true,:outputable=>true,:printable=>true
+      redirect_to :controller=>"model_search",:action=>"index",:class_name=>"DocHead",:joins=>"work_flow_infos",:pre_condition=>"work_flow_infos.people_id=#{current_user.person.id}",:lookup=>true,:title=>I18n.t("menu.docs_approved"),:layout=>true,:outputable=>true,:printable=>true
     else
       render "task/no_rights_error"
     end
@@ -34,7 +34,7 @@ class TaskController < ApplicationController
   #docs already paid
   def docs_paid
     if current_user.person
-      redirect_to :controller=>"model_search",:action=>"index",:class_name=>"DocHead",:pre_condition=>"doc_state=3",:lookup=>true,:title=>"已经付款的单据",:layout=>true,:outputable=>true,:printable=>true
+      redirect_to :controller=>"model_search",:action=>"index",:class_name=>"DocHead",:pre_condition=>"doc_state=3",:lookup=>true,:title=>I18n.t("menu.docs_paid"),:layout=>true,:outputable=>true,:printable=>true
     else
       render "task/no_rights_error"
     end
