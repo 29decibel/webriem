907140076d742ccacbb767f1c61c23065a9cfd11
dongbin.li <mike.d.1984@gmail.com>
2010-11-29 23:05:06 +0800

    changes

--- config/initializers/admin_data.rb
+++ config/initializers/admin_data.rb
@@ -1,4 +1,4 @@
 AdminData::Config.set = {
-  :is_allowed_to_view => lambda {|controller| current_user.name=="admin" },
-  :is_allowed_to_update => lambda {|controller| current_user.name=="admin"},
+  :is_allowed_to_view => lambda {|controller| return true },
+  :is_allowed_to_update => lambda {|controller| return true }
 }
