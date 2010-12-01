3b246078b2e932aac03aa4fae9b9c0bcb48b9dfc
dongbin.li <mike.d.1984@gmail.com>
2010-12-01 16:42:18 +0800

    changes

--- app/controllers/model_search_controller.rb
+++ app/controllers/model_search_controller.rb
@@ -15,8 +15,8 @@ class ModelSearchController < ApplicationController
     if params[:filter_method] and @class.respond_to?(params[:filter_method])
     	@results=@class.send(params[:filter_method],@results,current_user.person)
     end
-    render :text=>@results.map {|r| r.id }
-    #render :layout=>false if !params[:layout]
+    #render :text=>@results.map {|r| r.id }
+    render :layout=>false if !params[:layout]
   end
   def with
     @class=eval(params[:class_name])
