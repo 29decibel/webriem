265cffb276f3933c51109f524e840f48d8d8a03b
dongbin.li <mike.d.1984@gmail.com>
2010-12-02 16:57:17 +0800

    changes

--- app/models/work_flow_step.rb
+++ app/models/work_flow_step.rb
@@ -3,5 +3,5 @@ class WorkFlowStep < ActiveRecord::Base
   belongs_to :work_flow
   belongs_to :dep
   belongs_to :duty
-  enum_attr :is_self_dep, [["#{I18n.t('common_attr.__ok')}", 0], ["#{I18n.t('common_attr.sure_not')}", 1]]
+  enum_attr :is_self_dep, [["是", 0], ["否", 1]]
 end
