1e732c9e97779c5ab6379045ca127a8c56ad985d
dongbin.li <mike.d.1984@gmail.com>
2011-02-13 00:24:56 +0800

    update service api

--- app/models/fee_code_match.rb
+++ app/models/fee_code_match.rb
@@ -3,6 +3,6 @@ class FeeCodeMatch < ActiveRecord::Base
   belongs_to :ccode,:class_name=>"U8code",:foreign_key=>"ccode_id"
   belongs_to :dcode,:class_name=>"U8code",:foreign_key=>"dcode_id"
   def before_save
-    fee_code=fee.code
+    self.fee_code=fee.code
   end
 end
