440b9b656bf866d9491a7be7d2d28922a8e2af0d
dongbin.li <mike.d.1984@gmail.com>
2011-02-15 21:31:36 +0800

    update vouch form

--- app/models/vouch.rb
+++ app/models/vouch.rb
@@ -5,7 +5,7 @@ class Vouch < ActiveRecord::Base
   belongs_to :project,:class_name=>"Project",:foreign_key=>"item_id"
   belongs_to :code,:class_name=>"U8code",:foreign_key=>"code_id"
   belongs_to :code_equal,:class_name=>"U8code",:foreign_key=>"code_equal_id"
-  def cdep_id
+  def cdept_id
     dep==nil ? "" : dep.code
   end
   def citem_id
