b129f086c8ff699a23d2c4acedfd3800ab3c495e
dongbin.li <mike.d.1984@gmail.com>
2011-02-16 21:32:16 +0800

    update gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 #coding: utf-8
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate_user!,:except => [:sign_in, :sign_up]
+  #before_filter :authenticate_user!,:except => [:sign_in, :sign_up]
   def get_error_messages(record)
     error_msg="\\n"
     record.errors.full_messages.each do |msg|
