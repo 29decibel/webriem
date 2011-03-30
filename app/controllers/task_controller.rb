749df67ede0aba199bd972008c1593b77ce8fdc7
dongbin.li <dongbin.li@elong.corp.com>
2011-03-30 13:47:07 +0800

    changes

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,24 +1,37 @@
 #coding: utf-8
 class TaskController < ApplicationController
   def my_docs
-    render "task/no_rights_error" unless current_user.person
+    @docs=DocHead.by_person(current_user.person.id)
+    respond_to do |format|
+      format.js { render 'show_docs'}
+    end
   end
   #docs need to approve
   def docs_to_approve
-    render "task/no_rights_error" unless current_user.person
+    @docs=DocHead.by_person(current_user.person.id)
+    respond_to do |format|
+      format.js { render 'show_docs'}
+    end
   end
   #the docs need to pay
   def docs_to_pay
-    render "task/no_rights_error" unless current_user.person and 
-    current_user.person.person_type and 
-    current_user.person.person_type.code=="CA"
+    @docs=DocHead.by_person(current_user.person.id)
+    respond_to do |format|
+      format.js { render 'show_docs'}
+    end
   end
   #the docs that has been approved
   def docs_approved
-    render "task/no_rights_error" unless current_user.person
+    @docs=DocHead.by_person(current_user.person.id)
+    respond_to do |format|
+      format.js { render 'show_docs'}
+    end
   end
   #docs already paid
   def docs_paid
-    render "task/no_rights_error" unless current_user.person
+    @docs=DocHead.by_person(current_user.person.id)
+    respond_to do |format|
+      format.js { render 'show_docs'}
+    end
   end
 end
