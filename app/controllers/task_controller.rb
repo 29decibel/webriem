bffbd613219281e7ec57c55341c83bfaf901ff1b
dongbin.li <mike.d.1984@gmail.com>
2011-04-08 15:36:29 +0800

    some changes

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -9,9 +9,7 @@ class TaskController < ApplicationController
   end
   #the docs need to pay
   def docs_to_pay
-    render "task/no_rights_error" unless current_user.person and 
-    current_user.person.person_type and 
-    current_user.person.person_type.code=="CA"
+    render "task/no_rights_error" unless current_user.person 
   end
   #the docs that has been approved
   def docs_approved
