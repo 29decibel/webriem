0461d80b93c80b785c7bab87b714c77f44499423
dongbin.li <mike.d.1984@gmail.com>
2011-05-05 20:28:08 +0800

    add auto migrate plugin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110319051935) do
+ActiveRecord::Schema.define(:version => 20110505122222) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
