43ba09abfc1ae889570e03ccd2dc6a4e960123d8
dongbin.li <mike.d.1984@gmail.com>
2011-05-19 08:53:09 +0800

    fix some problems of using rails admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,9 +1,12 @@
 RailsAdmin.config do |config|
-  config.included_models = [Dep, Person, Currency,Project,Settlement,Duty,WorkFlow,WorkFlowStep]
+  config.included_models = 
+    [Dep, Person, Currency,Project,Settlement,
+    Duty,WorkFlow,WorkFlowStep,Role,Fee,Region,Transportation,Lodging,
+    FeeStandard,Budget,Account,Subject,ExtraWorkStandard,Menu,MenuCategory]
 
-  config.model Person do
-    label "Person Model"
-  end  
+  #config.model Person do
+  #  label "Person Model"
+  #end  
 
   config.navigation.max_visible_tabs 8
 
