d5b67415ed26937a3232ffdac01098ba73af0e6f
mike <mike.d.1984@gmail.com>
2011-05-30 22:06:13 +0800

    add gdzc priint

--- app/models/fixed_property.rb
+++ app/models/fixed_property.rb
@@ -2,4 +2,5 @@ class FixedProperty < ActiveRecord::Base
   belongs_to :keeper,:class_name=>"Person",:foreign_key=>"keeper_id"
   belongs_to :afford_dep, :class_name => "Dep", :foreign_key => "afford_dep_id"
   belongs_to :project
+  belongs_to :property_type
 end
