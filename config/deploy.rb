cac37392d104eaac3ab1666c4594399adf9c0b38
mike <mike.d.1984@gmail.com>
2011-06-17 12:29:55 +0800

    add deloy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 default_run_options[:pty] = true
 
-set :application, "set your application name here"
+set :application, "OES"
 set :repository,  "git@github.com:29decibel/webriem.git"
 set :user,"ldb"
 set :deploy_via, :remote_cache
@@ -16,9 +16,37 @@ role :db,  "oesnow.com", :primary => true # This is where Rails migrations will 
 # these http://github.com/rails/irs_process_scripts
 
 namespace :deploy do
-  set :deploy_to,"/home/ldb/app"
 
-  task :restart, :roles => :app do
+  desc "deploy the dev app"
+  task :dev do
+    # you just told the cap where you what to put , and then the current,release and shared folder will created
+    # for you
+    set :deploy_to, "/home/ldb/dev/oes"
+    set :branch, "dev"
+    set :env, "production"
+    
+    transaction do
+      update_code
+      symlink
+      copy_config
+      migrate
+    end
+
+    restart
+  end
+
+  desc "Rake database"
+  task :migrate do
+    run "cd #{deploy_to}/current && bundle install --deployment"
+    run "cd #{deploy_to}/current && RAILS_ENV=#{env} bundle exec rake db:schema:load"
+  end
+
+  desc "copy configs"
+  task :copy_config do
+    run "cp #{deploy_to}/shared/database.yml #{deploy_to}/current/config/"
+  end
+
+  task :restart do
     run "cd #{deploy_to}/current && touch tmp/restart.txt"
   end
 end
