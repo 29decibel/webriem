e0561b72d2394328e9bae8fae82a7f9ed492b0be
29decibel <mike.d.1984@gmail.com>
2011-07-26 07:45:46 +0800

    make doc rows searh better

--- app/controllers/doc_rows_controller.rb
+++ app/controllers/doc_rows_controller.rb
@@ -4,7 +4,7 @@ class DocRowsController < ApplicationController
     #if params[:search] 
       #params[:search].delete 'resource_type_eq' if params[:search]['resource_type_eq'].first.blank?
       @search = DocRow.search(params[:search])
-      @doc_rows = @search.all
+      @doc_rows = params[:search] ?  @search.all : []
     #end
   end
 
