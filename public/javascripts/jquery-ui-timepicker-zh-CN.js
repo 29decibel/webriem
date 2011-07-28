bf4dd9b89a3722c566c3388f8abad83a237f5985
29decibel <mike.d.1984@gmail.com>
2011-07-28 17:20:56 +0800

    fix datetime picker style

---
+++ public/javascripts/jquery-ui-timepicker-zh-CN.js
@@ -0,0 +1,17 @@
+/* Chinese translation for the jQuery Timepicker Addon */
+/* Written by 29decibel */
+(function($) {
+	$.timepicker.regional['zh-CN'] = {
+		timeOnlyTitle: '选择时间',
+		timeText: '时间',
+		hourText: '小时',
+		minuteText: '分钟',
+		secondText: '秒',
+		timezoneText: '时区',
+		currentText: '当前时间',
+		closeText: '关闭',
+		timeFormat: 'hh:mm',
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['zh-CN']);
+})(jQuery);
