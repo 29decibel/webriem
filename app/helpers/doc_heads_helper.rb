cda1c4a732b8ef31bb313849541cc9508c3c3104
29decibel <mike.d.1984@gmail.com>
2011-07-30 19:47:26 +0800

    add person name show on doc list

--- app/helpers/doc_heads_helper.rb
+++ app/helpers/doc_heads_helper.rb
@@ -1,8 +1,8 @@
 #coding: utf-8
 module DocHeadsHelper
   def can_adjust_amount(doc,person_type)
-    # doc.current_approver_id == current_user.person.id and
+    doc.current_approver_id == current_user.person.id and
     current_user.person.person_type and 
-    current_user.person.person_type.code == person_type
+    current_user.person.person_type.code == person_type and doc.processing?
   end
 end
