70d256113f8e52adef7ea00ffda54a6e485d2f48
29decibel <mike.d.1984@gmail.com>
2011-10-18 13:33:47 +0800

    update deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,15 +2,15 @@ default_run_options[:pty] = true
 
 set :application, "OES"
 set :repository,  "git@github.com:29decibel/webriem.git"
-set :user,"ldb"
+set :user,"fin"
 set :deploy_via, :remote_cache
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "oesnow.com"                  ,:primary => true        # Your HTTP server, Apache/etc
-role :app, "oesnow.com"                          # This may be the same as your `Web` server
-role :db,  "oesnow.com", :primary => true # This is where Rails migrations will run
+role :web, "211.154.169.179"                  ,:primary => true        # Your HTTP server, Apache/etc
+role :app, "211.154.169.179"                          # This may be the same as your `Web` server
+role :db,  "211.154.169.179", :primary => true # This is where Rails migrations will run
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -21,8 +21,8 @@ namespace :deploy do
   task :dev do
     # you just told the cap where you what to put , and then the current,release and shared folder will created
     # for you
-    set :deploy_to, "/home/ldb/dev/oes"
-    set :branch, "origin/dev"
+    set :deploy_to, "/home/fin/app"
+    set :branch, "origin/vrv"
     set :env, "production"
     
     transaction do
