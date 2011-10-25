0f9f84aec7f1a26e83cabaaa5a005df4b06b862e
29decibel <mike.d.1984@gmail.com>
2011-10-25 14:33:50 +0800

    update select approver and min-height

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111024091319) do
+ActiveRecord::Schema.define(:version => 20111025062508) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
