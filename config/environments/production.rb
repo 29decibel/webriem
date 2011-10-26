1a630e0dfa99c4df0e04feca9d11db02b7f3bcdb
dongbin.li <mike.d.1984@gmail.com>
2011-10-26 13:37:12 +0800

    fix active_admin compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,7 @@ Webreim::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.precompile += %w[active_admin.css active_admin.js]
 
   # Generate digests for assets URLs
   config.assets.digest = true
