e5552dad980dde5fc8f58fdb2b2688cc32d35766
29decibel <mike.d.1984@gmail.com>
2011-10-28 14:17:08 +0800

    rename u8service

--- app/vender_service/u8_service.rb
+++ app/vender_service/u8_service.rb
@@ -1,9 +1,5 @@
 #coding: utf-8
-class U8Vouch
-  ProjectsServiceURL="http://gpm.skcc.com/getAllProjectsInformationFromGPM.do"
-  EmployeesServiceURL="http://10.120.108.97:7001/web2011/GetEmployeeInformations.do"
-  U8ServiceURL="http://10.120.128.28:8008/Service1.asmx"
-
+class U8Service
   #def self.test_g_vouch
   #  options={
   #    :ino_id=>"10000",:inid=>"4444",:dbill_date=>"2011-3-1",
@@ -36,8 +32,6 @@ class U8Vouch
     result_msg
   end
 
-
-
   def self.exec_sql(sql)
     client = TinyTds::Client.new(:host=>'10.120.128.28',:database=>'UFDATA_500_2011',:username=>'sa',:password=>'',:encoding=>'GBK')
     if sql.start_with? 'select'
@@ -67,32 +61,7 @@ class U8Vouch
     select_cmd = "select max(ino_id) from GL_accvouch where iperiod='#{Time.now.month}'"
     self.exec_sql(select_cmd).first[""]
   end
-  def self.get_codes
-    JSON get("GetCodes")
-  end
-  def self.get_person
-    JSON get("GetPerson")
-  end
-  def self.get_departments
-    JSON get("GetDepartment")
-  end
-  def self.get_projects
-    JSON get("GetProjects")
-  end
-  def self.get_currency
-    JSON get("GetCurrency")
-  end
-  def self.get(service_name,options={})
-    if RAILS_ENV=="development"
-      return "{}"
-    end
-    _dbname =options[:dbname] || dbname
-    para={:dbname=>_dbname}
-    para.merge! options.except(:dbname)
-    #para=para.merge options.except()
-    response = RestClient.post "#{U8ServiceURL}/#{service_name}", para.to_json, :content_type => :json, :accept => :json
-    return JSON(response.body)["d"]
-  end
+
   #here is the config info of current year's info
   def self.dbname
     config=SystemConfig.find_by_key("u8dbname")
@@ -101,53 +70,5 @@ class U8Vouch
     end
     Rails.logger.error("请配置u8数据库的名称")
     ""
-  end
-  #<name>高波</name>
-  #<empno>CS10065</empno>
-  #<gender>女</gender>
-  #<deptid>100103</deptid>
-  #deptName
-  #<mobile>131-2049-7186</mobile>
-  #<email>gaobo@skccsystems.cn</email>
-  #idCard
-  #bankid
-  def self.get_hr_employees
-    emps=[]
-    begin
-      response=RestClient.get EmployeesServiceURL
-      xml_doc=REXML::Document.new response.to_s
-      xml_doc.root.elements.each do |ele|
-        p=Person.new
-        p.name=ele.elements["name"].text.strip if ele.elements["name"]
-        p.code=ele.elements["empno"].text.strip if ele.elements["empno"]
-        p.gender=ele.elements["gender"].text=="男" ? 1 : 0 if ele.elements["gender"]
-        p.dep_id=ele.elements["deptid"].text.strip if ele.elements["deptid"]
-        p.phone=ele.elements["mobile"].text.strip if ele.elements["mobile"]
-        p.e_mail=ele.elements["email"].text.strip if ele.elements["email"]
-        p.bank_no=ele.elements["bankid"].text.strip if ele.elements["bankid"]
-        p.ID_card=ele.elements["idCard"].text.strip if ele.elements["idCard"]
-        emps<<p
-      end
-    rescue Exception=>msg
-      Rails.logger.error "can't get the employees info from hr system,errors is #{msg}"
-    end
-    return emps
-  end
-  def self.get_gpm_projects
-    projects=[]
-    begin
-      response=RestClient.get ProjectsServiceURL
-      xml_doc=REXML::Document.new response.to_s
-      xml_doc.root.elements.each do |ele|
-        p=Project.new
-        p.name=ele.elements["prjName"].text.strip
-        p.code=ele.elements["prjId"].text.strip
-        p.status=1
-        projects<<p
-      end
-    rescue Exception=>msg
-      Rails.logger.error "can't get the projects from gpm,errors is #{msg}"
-    end
-    return projects
   end
 end
