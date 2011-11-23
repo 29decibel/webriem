17dcddb97ebf63e7a6a3387a1ee9a54bcfcfee91
dongbin.li <mike.d.1984@gmail.com>
2011-11-23 21:59:42 +0800

    support solo menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,8 @@
 #coding: utf-8
 class Menu < ActiveRecord::Base
+  scope :solo,where("group_name is null or group_name=''")
+  scope :has_group,where("group_name is not null and group_name!=''")
+
   validates_uniqueness_of :name
 
   has_and_belongs_to_many :roles
