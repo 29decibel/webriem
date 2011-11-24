6ecdf214421e1c6c25995c2c2beea0dc78459b56
29decibel <mike.d.1984@gmail.com>
2011-11-24 15:40:48 +0800

    fix doc meta info

--- app/models/doc_meta_info.rb
+++ app/models/doc_meta_info.rb
@@ -2,4 +2,9 @@ class DocMetaInfo < ActiveRecord::Base
   has_many :doc_relations
   has_many :doc_row_meta_infos,:through => :doc_relations
   accepts_nested_attributes_for :doc_relations , :allow_destroy => true
+
+  def doc_head_attrs
+    self[:doc_head_attrs].split(',') if self[:doc_head_attrs]	
+  end
+
 end
