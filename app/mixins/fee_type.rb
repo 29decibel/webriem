beb4bc77aa65acb8abd6cb9da5f8fc0ff229bc7d
dongbin.li <mike.d.1984@gmail.com>
2011-11-30 10:21:08 +0800

    fix config fee

--- app/mixins/fee_type.rb
+++ app/mixins/fee_type.rb
@@ -2,7 +2,9 @@ module FeeType
   def self.included(klass)
     klass.class_eval <<-EOF
       def fee_type
-        self.respond_to?(:fee) ? self.fee : DocRowMetaInfo.find_by_name(self.class.name).try(:fee)
+        config_fee = DocRowMetaInfo.find_by_name(self.class.name).try(:fee)
+        return config_fee if config_fee
+        self.respond_to?(:fee) ? self.fee : config_fee
       end
     EOF
   end
