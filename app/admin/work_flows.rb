9ecd54745abab654489b6a02908359d01878bbe0
dongbin.li <mike.d.1984@gmail.com>
2011-12-07 21:22:01 +0800

    update admin

--- app/admin/work_flows.rb
+++ app/admin/work_flows.rb
@@ -32,10 +32,11 @@ ActiveAdmin.register WorkFlow do
     f.inputs "审批环节(请确保第一个审批环节为一个人，否则员工无法提交审批)" do
       f.has_many :work_flow_steps,:label=>'添加审批环节' do |p|
         p.input :factors,:hint=>'职务:总裁助理,部门:金融能源,姓名:张三（条件之间使用逗号隔开，冒号和逗号都需要为半角字符）'
-        p.input  :step_code
+        #p.input  :step_code
         p.input  :is_self_dep,:hint=>'定位部门顺序：费用承担部门-->所代申请人所在部门-->申请人所在部门'
-        p.input  :can_change_approver_steps
-        p.input  :max_amount
+        #p.input  :can_change_approver_steps
+        #p.input  :max_amount
+        p.input  :can_skip
         p.input  :can_change_amount
         p.input  :_destroy, :as => :boolean, :label => "删除此环节" unless p.object.new_record?
         p.form_buffers.last
