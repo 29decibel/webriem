5dadfba8b34e31755bc2b03a0bc65e30497e8e87
dongbin.li <mike.d.1984@gmail.com>
2011-12-07 13:34:43 +0800

    update send ht to u8

--- app/models/contract_doc.rb
+++ app/models/contract_doc.rb
@@ -10,7 +10,7 @@ class ContractDoc < ActiveRecord::Base
 
   def customer_code
     if self.source=='直单'
-      sql = "SELECT  cCusCode as code, cCusName as name FROM Customer where cCusName=#{customer_name}"
+      sql = "SELECT  cCusCode as code, cCusName as name FROM Customer where cCusName='#{customer_name}'"
       result = U8Service.exec_sql sql
       result.first['code']
     else
