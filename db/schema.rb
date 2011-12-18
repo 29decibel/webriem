93c3f73b78bd7620f5c4369913296e75cd6b0871
dongbin.li <mike.d.1984@gmail.com>
2011-12-18 09:24:20 +0800

    add attachemnts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111218010525) do
+ActiveRecord::Schema.define(:version => 20111218012351) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
@@ -316,6 +316,7 @@ ActiveRecord::Schema.define(:version => 20111218010525) do
     t.boolean  :support_vouch
     t.boolean  :payable,:default=>true
     t.string   "description"
+    t.boolean   "attachments"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
